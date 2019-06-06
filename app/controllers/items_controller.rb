1b6186cc39d6469507304110b75b2e98a4736351
johnhaus <john.haus@outlook.com>
2019-06-06 11:54:19 -0300

    configured controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,49 @@
 class ItemsController < ApplicationController
+  before_action :set_items, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+
+    if @item.save
+      redirect_to @item, notice: "Item successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: "Item successfully updated."
+    else
+      render :edit
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_url, notice: "Item successfully deleted."
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name)
+    end
 end
